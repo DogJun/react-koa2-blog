1839df0bdc8f5b97d009941f0de15d2d4b384e3a
DogJun <498343874@qq.com>
2019-09-25 23:49:21 +0800

    chore: update webpack config

---
+++ config/webpack.development.js
@@ -0,0 +1,19 @@
+const { join, resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  output: {
+    filename: "scripts/[name].bundule.js"
+  },
+  devServer: {
+    hot: true,
+    contentBase: join(__dirname, '../dist')
+  },
+  plugins: [
+    // new HtmlWebpackPlugin({
+    //   title: "DogJun's Blog",
+    //   filename: "index.html",
+    //   template: resolve(__dirname, "../src/client/index-dev.html")
+    // }),
+    new HtmlWebpackPlugin()
+  ]
+}
